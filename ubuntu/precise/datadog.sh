c3cdeaeeab7e64dab27ec88fb1b04670e6b16587
Mike McClenaghan <mike@sideline.ca>
2013-04-12 09:17:27 -0600

    Updated datadog script

--- ubuntu/precise/datadog.sh
+++ ubuntu/precise/datadog.sh
@@ -1,9 +1,19 @@
-# Install datadog 
+# Install datadog
 #
-# $1 - datadog_api_key
+# Environment variables required:
+#
+# DATADOG_API_KEY           # should be same as $APP_NAME
+# DRY_RUN                   # if set, don't execute install
 
-if dpkg -s "datadog-agent"; then
-  echo 'datadog already installed'
-else
+function check_datadog() {
+  dpkg -s "datadog-agent"  > /dev/null 2>&1
+}
+
+function install_datadog() {
   DD_API_KEY=$1 bash -c "$(curl -L http://dtdg.co/agent-install-ubuntu)"
+}
+
+if ! check_datadog; then
+  echo "Installing datadog agent"
+  [[ $DRY_RUN ]] || install_datadog "$DATADOG_API_KEY"
 fi
