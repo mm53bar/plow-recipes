488584a4093e47812ded8a5dfc7edb8c5bb824b3
Mike McClenaghan <mike@sideline.ca>
2013-04-12 11:02:45 -0600

    Updated rails_app recipe

--- ubuntu/precise/rails_app.sh
+++ ubuntu/precise/rails_app.sh
@@ -1,25 +1,40 @@
-# Configure the rails app for cap deploy:setup
+# Configure misc stuff for a Rails app
 #
-# $1 - deployer
-# $2 - app_name
-# $3 - git_host
+# Environment variables required:
 #
-# requires:
-#   deployer
+# DEPLOYER                  # should be same as $APP_NAME
+# GIT_HOST                  # ex. github.com
+# DRY_RUN                   # if set, don't execute install
+#
+# Recipes required:
+#
+# deployer
 
+function check_app_folder() {
+  test -d /srv/$1
+}
 
-if test -d /var/applications/$2 ; then
-  echo 'App folder already created'
-else
-  mkdir -p /var/applications/$2
-  chown $1 /var/applications/$2
-fi
+function create_app_folder() {
+  mkdir -p /srv/$1
+  chown $1 /srv/$1
+}
 
-if [[ -e /home/$1/.ssh/known_hosts ]] && 
-   grep -Fq $3 /home/$1/.ssh/known_hosts ; then
-  echo 'Git host already initialized'
-else
-  ssh-keyscan $3 | tee /home/$1/.ssh/known_hosts
+function check_git_host() {
+  test -e /home/$1/.ssh/known_hosts && 
+     grep -Fq $2 /home/$1/.ssh/known_hosts
+}
+
+function create_git_host() {
+  ssh-keyscan $2 | tee /home/$1/.ssh/known_hosts
   chown $1:$1 /home/$1/.ssh/known_hosts
+}
+
+if ! check_app_folder; then
+  echo "Creating folder for '$DEPLOYER'"
+  [[ $DRY_RUN ]] || create_app_folder "$DEPLOYER"
 fi
 
+if ! check_git_host; then
+  echo "Adding git host"
+  [[ $DRY_RUN ]] || create_git_host "$DEPLOYER" "$GIT_HOST"
+fi
