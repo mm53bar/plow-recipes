721e1f71df1196dcf1909804b604f1d55c0db0ce
Mike McClenaghan <mike@sideline.ca>
2013-04-12 09:49:21 -0600

    Updated node.js recipe

--- ubuntu/precise/nodejs.sh
+++ ubuntu/precise/nodejs.sh
@@ -1,10 +1,21 @@
-# Install node.js 
+# Install node.js
+#
+# Environment variables required:
+#
+# DRY_RUN                   # if set, don't execute install
 
-if dpkg -s "nodejs"; then
-  echo 'node.js already installed'
-else
+function check_nodejs() {
+  dpkg -s "nodejs"  > /dev/null 2>&1
+}
+
+function install_nodejs() {
   apt-get install -y python-software-properties
   add-apt-repository ppa:chris-lea/node.js
   apt-get update
   apt-get install -y nodejs
+}
+
+if ! check_nodejs; then
+  echo "Installing node.js"
+  [[ $DRY_RUN ]] || install_nodejs
 fi
